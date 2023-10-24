1907fd3675a1e94810e4af9d50d15b99c6eae15d
gruszm <mariuszgruszczynski11@gmail.com>
2023-10-24 20:14:23 +0200

    Home activity added

---
+++ app/src/main/java/com/example/kalkulator/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.kalkulator;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.kalkulator.activities.MainActivity;
+
+public class HomeActivity extends AppCompatActivity
+{
+    private Button btnSimpleCalculator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnSimpleCalculator = findViewById(R.id.btn_simple_calculator);
+        btnSimpleCalculator.setOnClickListener(this::btnSimpleCalculatorOnClick);
+    }
+
+    private void btnSimpleCalculatorOnClick(View v)
+    {
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
