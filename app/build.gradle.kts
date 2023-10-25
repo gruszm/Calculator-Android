5c5dd95cb1c92a9e38186a33a06dfb0be45d8975
gruszm <mariuszgruszczynski11@gmail.com>
2023-10-25 18:11:33 +0200

    Home activity - welcome message, simple calc button enlarged

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha02")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha08")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
