8a3f52541a66ab30cda66e28f4cf546ab1d07f24
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-01 23:22:37 +0100

    Portrait layout created with placeholder buttons

---
+++ app/src/main/java/com/example/kalkulator/AdvancedCalcActivity.java
@@ -0,0 +1,15 @@
+package com.example.kalkulator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdvancedCalcActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced_calc);
+    }
+}
